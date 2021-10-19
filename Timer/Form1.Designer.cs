5e4aa6b7de0c5f0a45732a48b7b39528b1934e0f
krgulnaz <krgulnoza@gmail.com>
2021-10-19 12:14:09 +0200

    Added initial commit, controls and logic

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 68);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(102, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ".  00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(67, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":  00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(43, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 95);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(85, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(107, 95);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(85, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 129);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stopwatch";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Form1 : Form
+    {
+        private int minute, second, milliSecond;
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (this.timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                startButton.Text = "Start";
+                stopButton.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = true;
+                startButton.Text = "Stop";
+                stopButton.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (milliSecond == 99)
+            {
+                if (second == 59)
+                {
+                    if (minute == 99)
+                    {
+                        minute = 0;
+                        second = 0;
+                    }
+                }
+                else
+                {
+                    second++;
+                    milliSecond = 0;
+                }
+            }
+            else
+            {
+                milliSecond++;
+            }
+
+
+            if (minute.ToString().Length == 1)
+            {
+                label1.Text = "0" + minute.ToString();
+            }
+            else
+            {
+                label1.Text =minute.ToString();
+            }
+
+            if (second.ToString().Length == 1)
+            {
+                label2.Text = "0" + second.ToString();
+            }
+            else
+            {
+                label2.Text = second.ToString();
+            }
+
+            if (milliSecond.ToString().Length == 1)
+            {
+                label3.Text = "0" + milliSecond.ToString();
+            }
+            else
+            {
+                label3.Text = milliSecond.ToString();
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            this.minute = 0;
+            this.second = 0;
+            this.milliSecond = 0;
+            this.label1.Text = "00";
+            this.label2.Text = "  :00";
+            this.label3.Text = ".  00";
+        }
+
+        //private System.Windows.Forms.Timer timer= new System.Windows.Forms.Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            minute = 0;
+            second = 0;
+            milliSecond = 0;
+            timer1.Interval = 10;
+        }
+    }
+}
